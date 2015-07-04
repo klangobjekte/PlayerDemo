9adf93f647ce604877561c2c81f33c5d62693e49
Manfred Schmid <Manfred@klangobjekte.com>
2015-07-04 15:07:04 +0200

    QT_VERSION_CHECK FIX

--- widget.cpp
+++ widget.cpp
@@ -439,7 +439,7 @@ void PlayerWidget::on_pausePushButton_clicked(){
 
 void PlayerWidget::on_devicesComboBox_currentIndexChanged(QString id){
     int _id =0;
-#if QT_VERSION > 0x040000
+#if (QT_VERSION >= QT_VERSION_CHECK(5,0,0))
     //! c++11
     for(auto iter: outDevices){
         if(iter.second == id.toStdString().c_str()){
